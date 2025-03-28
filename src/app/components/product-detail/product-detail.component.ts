6ef117602ac91e6fcba18801192ce713172929d8
Michael-Melad <michaelmeladkoko55@gmail.com>
2025-03-28 23:25:35 +0200

    Add missing files

---
+++ src/app/components/product-detail/product-detail.component.ts
@@ -0,0 +1,30 @@
+
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../../services/product.service';
+
+@Component({
+  selector: 'app-product-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+  product: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.productService.getProductById(+id).subscribe((data) => {
+        this.product = data;
+      });
+    }
+  }
+}
